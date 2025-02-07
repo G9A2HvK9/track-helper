1c1da9645c96f372f9c06edf6605be4c563476df
radhouane12 <rhimi.red1@gmail.com>
2025-02-08 00:42:25 +0100

    Stop tracking local.db and split models

--- backend/data/models.py
+++ backend/data/models.py
@@ -5,7 +5,7 @@ from backend.database import Base
 # ✅ Fix: Prevent Duplicate Table Definition by using `extend_existing=True`
 tags_association = Table(
     'tags_association', Base.metadata,
-    Column('owned_music_id', Integer, ForeignKey('owned_music.id')),
+    Column('owned_music_losless_id', Integer, ForeignKey('owned_music_losless.id')),
     Column('tag_id', Integer, ForeignKey('tags.id')),
     extend_existing=True  
 )
@@ -19,19 +19,28 @@ class Tag(Base):
     def __repr__(self):
         return f"<Tag(name={self.name})>"
 
-class OwnedMusic(Base):
-    __tablename__ = 'owned_music'
+class OwnedMusicLosless(Base):
+    __tablename__ = 'owned_music_losless'
     
     id = Column(Integer, primary_key=True, index=True)
     artist = Column(String, index=True)
     track_id = Column(String, unique=True, index=True)
-    format = Column(String)
     
     # ✅ Many-to-many relationship with Tag
-    tags = relationship("Tag", secondary=tags_association, backref="owned_music")
+    tags = relationship("Tag", secondary=tags_association, backref="owned_music_losless")
 
     def __repr__(self):
-        return f"<OwnedMusic(artist={self.artist}, track_id={self.track_id}, format={self.format}, tags={self.tags})>"
+        return f"<OwnedMusic(artist={self.artist}, track_id={self.track_id}, tags={self.tags})>"
+
+class OwnedMusicCompressed(Base):
+    __tablename__ = 'owned_music_compressed'
+    
+    id = Column(Integer, primary_key=True, index=True)
+    artist = Column(String, index=True)
+    track_id = Column(String, unique=True, index=True)
+
+    def __repr__(self):
+        return f"<OwnedMusic(artist={self.artist}, track_id={self.track_id})>"
 
 class UnownedMusic(Base):
     __tablename__ = 'unowned_music'
